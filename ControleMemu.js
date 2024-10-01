54ac8954947e0933c6f97b85c0cd039e85ef76b1
gs4vo <gustavo.ferreira@alunos.bpkedu.com.br>
2024-10-01 14:17:41 -0300

    v1

---
+++ ControleMemu.js
@@ -0,0 +1,55 @@
+const fs = require('fs').promises; // Usar a versão promissora do fs
+const path = require('path');
+const { exec } = require('child_process');
+
+function openApp(appPath) {
+    return new Promise((resolve, reject) => {
+        exec(`start "" "${appPath}"`, (error) => {
+            if (error) {
+                console.error(`Erro ao abrir o aplicativo: ${error.message}`);
+                return reject(error);
+            }
+            console.log(`Aplicativo aberto: ${appPath}`);
+            resolve();
+        });
+    });
+}
+
+function closeApp(appPath) {
+    return new Promise((resolve, reject) => {
+        const appName = path.basename(appPath);
+        exec(`taskkill /IM "${appName}" /F`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Erro ao fechar o app: ${error.message}`);
+                return reject(error);
+            }
+            if (stderr) {
+                console.error(`Erro: ${stderr}`);
+                return reject(new Error(stderr));
+            }
+            console.log(`App fechado: ${stdout}`);
+            resolve();
+        });
+    });
+}
+
+async function runAllExecutablesInFolder(folderPath) {
+    try {
+        const files = await fs.readdir(folderPath);
+        const exeFiles = files.filter(file => file.endsWith('.exe'));
+
+        for (const file of exeFiles) {
+            const appPath = path.join(folderPath, file);
+            await openApp(appPath);
+            await new Promise(resolve => setTimeout(resolve, 10000));
+            await closeApp(appPath);
+        }
+
+        console.log('Todos os aplicativos foram processados.');
+    } catch (err) {
+        console.error(`Erro ao processar a pasta: ${err.message}`);
+    }
+}
+
+const folderPath = "C:\\Users\\gs4vo\\AppData\\Local\\Postman";
+runAllExecutablesInFolder(folderPath);
